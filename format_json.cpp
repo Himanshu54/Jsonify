4996d2b83de7ee3d67777a1fe58a36690df96bad
Himanshu <himu5493@gmail.com>
2019-11-27 15:16:47 +0530

    add cpp algo file

---
+++ format_json.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stack>
+
+#define TABS 4
+#define spaces(n) std::string(n, ' ')
+
+void print_json(int, std::string);
+void tokenizer(std::string);
+void parse_file(std::string);
+void read_file(std::string);
+
+int main(int argc, char **argv)
+{
+    read_file(argv[1]);
+}
+
+void print_json(int blanks, std::string json)
+{
+    int quoted = 0;
+    for (std::string::iterator it = json.begin(); it != json.end(); it++)
+    {
+        if (!quoted)
+        {
+            if (*it == '\n')
+                continue;
+            if (*it == '{' || *it == '[' || *it == '(')
+            {
+                blanks += TABS;
+                std::cout << *it << "\n"
+                          << spaces(blanks);
+            }
+            else if (*it == '}' || *it == ']' || *it == ')')
+            {
+                blanks -= TABS;
+                std::cout << "\n"
+                          << spaces(blanks) << *it;
+                std::cout << "\n"
+                          << spaces(blanks);
+            }
+            else if (*it == ',')
+            {
+                std::cout << *it << "\n"
+                          << spaces(blanks);
+                while (it != json.end() && *it == ' ')
+                {
+                    it++;
+                }
+            }
+            else if (*it == '"')
+            {
+                quoted = 1 - quoted;
+                std::cout<<*it;
+            }
+            else
+            {
+                std::cout<<*it;
+            }
+        }
+        else
+        {
+            if (*it == '"')
+            {
+                quoted = 1 - quoted;
+            }
+            std::cout << *it;
+        }
+    }
+}
+
+void read_file(std::string filename)
+{
+    int blanks = 0;
+    std::ifstream is(filename, std::ifstream::binary);
+
+    while (!is.eof())
+    {
+        std::string json_line;
+        getline(is, json_line);
+        print_json(blanks, json_line);
+    }
+}
